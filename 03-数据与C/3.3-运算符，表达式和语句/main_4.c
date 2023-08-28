651854f2cd156dd00ff9c6ba569ff5b93f0d0f28
yulin-Chen0507 <yulin.chen0507@gmail.com>
2023-08-28 14:55:59 +0800

    补充03数据与c 3.3-运算符，表达式和语句

---
+++ 03-数据与C/3.3-运算符，表达式和语句/main_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>//计算1～20的平方
+int main(void)
+{
+    int num = 1;
+    while (num < 21)
+    {
+        printf("%4d %6d\n", num, num * num);
+        num = num + 1;
+    }
+    return 0;
+}
